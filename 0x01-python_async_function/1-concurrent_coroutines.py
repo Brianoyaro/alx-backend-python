88d3c69e6e433a902e9f01e76c2e3375d54b6e34
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-06 14:58:38 +0300

    task 1 and task 4 should not use sort()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,13 @@ from typing import List
 wr = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns a list after running async tasks n times"""
-    res = await asyncio.gather(*(wr(max_delay) for _ in range(n)))
-    return sorted(res)
+    # res = await asyncio.gather(*(wr(max_delay) for _ in range(n)))
+    # return sorted(res)
+    tasks = [wr(max_delay) for _ in range(n)]
+    array = []
+    for task in asyncio.as_completed(tasks):
+        val = await task
+        array.append(val)
+    return array
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,10 +2,16 @@
 """executing multiple coroutines"""
 import asyncio
 from typing import List
-task_wait_random = __import__('3-tasks').task_wait_random
+wait = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """runs a concurrent task n times and returns a list of values"""
-    rs = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
-    return sorted(rs)
+    # rs = await asyncio.gather(*(wait(max_delay) for _ in range(n)))
+    # return sorted(rs)
+    tasks = [wait(max_delay) for _ in range(n)]
+    array = []
+    for task in asyncio.as_completed(tasks):
+        val = await task
+        array.append(val)
+    return array
