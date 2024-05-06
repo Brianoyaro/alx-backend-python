e3f3ac3263ffbc5a541443b177a4d1787017df49
brian <brian@brian.lan>
2024-05-06 13:46:06 +0300

    pycodestyled the files

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """runs an async function and retuns delay duration"""
     val = random.uniform(0, max_delay)
     await asyncio.sleep(val)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """executing multiple coroutines"""
 import asyncio
-wr = __import__('0-basic_async_syntax').wait_random
 from typing import List
+wr = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List:
