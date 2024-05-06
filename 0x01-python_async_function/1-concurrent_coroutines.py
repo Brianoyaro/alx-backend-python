689d3c0e5fc4b039a27b49447f844cbb28c6119a
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-06 14:07:00 +0300

    sorted return values of task 1 and task 4

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,4 +8,4 @@ wr = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List:
     """returns a list after running async tasks n times"""
     res = await asyncio.gather(*(wr(max_delay) for _ in range(n)))
-    return res
+    return sorted(res)
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,4 +8,4 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def task_wait_n(n: int, max_delay: int) -> List:
     """runs a concurrent task n times and returns a list of values"""
     rs = await asyncio.gather(*(task_wait_random(max_delay) for _ in range(n)))
-    return rs
+    return sorted(rs)
