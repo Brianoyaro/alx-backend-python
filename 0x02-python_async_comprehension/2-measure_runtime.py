471cfb50e8de9a0ed52ba089e54a99e86569a0ab
Brianoyaro <brianoyaro2000@yahoo.com>
2024-05-07 12:22:58 +0300

    initial commit

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,8 +8,7 @@ comp = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measurs time taken running 4 async comprehensions"""
     start = time.perf_counter()
-    tasks = asyncio.gather(comp(), comp(), comp(), comp())
-    await asyncio.run(tasks)
+    await asyncio.gather(comp(), comp(), comp(), comp())
     stop = time.perf_counter()
     time_taken = stop - start
     return time_taken
